a37baa266d2a32dffa87c01110432699aac4808c
fredluggard <fredrick.ebuka33@gmail.com>
2024-07-03 00:00:00 +0100

    Mobile desplay done

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-function handleActiveLink(event) {
-  event.preventDefault();
-
-  let activeLink = document.querySelectorAll(".tab");
-
-  activeLink.forEach((tab) => {
-    tab.classList.remove("active-link");
-  });
-
-  event.currentTarget.classList.add("active-link");
-}
-
 let currentDate = new Date();
 
 let hours = currentDate.getUTCHours();
@@ -33,10 +21,31 @@ let hamburgerLink1 = document.querySelector(".bum1");
 let hamburgerLink2 = document.querySelector(".bum2");
 let hamburgerLink3 = document.querySelector(".bum3");
 
+let activeMenu = document.querySelector(".overlay");
+
+let activeMenuLinks = document.querySelectorAll(".tab");
+
 hamburger.addEventListener("click", () => {
   document.querySelector(".bum1").classList.toggle("ham1");
 
   document.querySelector(".bum2").classList.toggle("ham2");
 
   document.querySelector(".bum3").classList.toggle("ham3");
+
+  document.body.classList.toggle("no-scroll");
+
+  activeMenu.classList.toggle("overlay-active");
+});
+
+activeMenuLinks.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    document.querySelector(".bum1").classList.toggle("ham1");
+
+    document.querySelector(".bum2").classList.toggle("ham2");
+
+    document.querySelector(".bum3").classList.toggle("ham3");
+
+    document.body.classList.toggle("no-scroll");
+    activeMenu.classList.toggle("overlay-active");
+  });
 });
